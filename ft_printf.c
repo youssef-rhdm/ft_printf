b9253e045367b6c29d9127aeb72520691cdb5e7c
youssef <76839145+youssef-rhdm@users.noreply.github.com>
2024-12-05 19:17:12 +0100

    quic setup

---
+++ ft_printf.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 14:50:22 by yrhandou          #+#    #+#             */
+/*   Updated: 2024/12/05 14:50:22 by yrhandou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 14:39:40 by yrhandou          #+#    #+#             */
+/*   Updated: 2024/12/05 14:47:42 by yrhandou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+static int check_format(va_list va, const char *str, int *counter)
+{
+	if (*str == 's')
+		counter += ft_putstr(va_arg(va, char *));
+	else if (*str == 'c')
+		counter += ft_putchar(va_arg(va, int));
+	else if (*str == 'p')
+		counter += ft_putstr("POINTER");
+	else if (*str == 'd')
+		counter += ft_putnbr(va_arg(va, int));
+	else if (*str == 'i')
+		counter += ft_putnbr(va_arg(va, int));
+	else if (*str == 'u')
+		counter += ft_putnbr(va_arg(va, int));
+	else if (*str == 'x')
+		counter += ft_puthex(va_arg(va, unsigned int), 1);
+	else if (*str == 'X')
+		counter += ft_putstr("UPPER HEX");
+	else if (*str == '%')
+		counter += ft_putchar(*str);
+	return *counter;
+}
+
+// int ft_printf_old(const char *str, ...)
+// {
+// 	va_list va;
+// 	int i;
+// 	int count;
+
+// 	i = 0;
+// 	count = 0;
+// 	va_start(va, str);
+// 	while (str[i] != '\0')
+// 	{
+// 		if (str[i] == '%')
+// 			count = check_format(va, &str[++i], &count);
+// 		else
+// 			ft_putchar(str[i]);
+// 		i++;
+// 		count++;
+// 	}
+// 	va_end(va);
+// 	return (i);
+// }
+
+int ft_printf(const char *str, ...)
+{
+	int counter;
+	int	i;
+	va_list	va;
+	i = 0;
+
+	counter = 0;
+	va_start(va, str);
+	while (str[i])
+	{
+		if (str[i]=='%')
+			counter += check_format(va ,&str[++i], &counter);
+		counter+= ft_putchar(str[i]);
+		i++;
+	}
+	return counter;
+}
+#include "libftprintf.h"
+#include <libc.h>
+
+int main(void)
+{
+	char str[] = "Sarah";
+	printf("------------------\n");
+	printf("Hello, my name is %s, I'm %d years old , My Name starts with %c , my name's address is %p  .\n", str, 26, str[0], &str[0]);
+	printf("------------------\n");
+	ft_printf("Hello, my name is %s, I'm %d years old , My Name starts with %c , my name's address is %p  .\n", str, 26, str[0], &str[0]);
+	printf("------------------\n");
+
+	return 0;
+}
---
+++ ft_puthex.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 11:29:50 by yrhandou          #+#    #+#             */
+/*   Updated: 2024/12/05 15:47:30 by yrhandou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+static int count_digits(unsigned int nbr)
+{
+	int count;
+
+	count = 1;
+	while (nbr / 10 != 0)
+	{
+		count++;
+	}
+	return (count);
+}
+void ft_puthex_upper_old(unsigned int nbr)
+{
+	int counter;
+	char *hex_list;
+	char remainders[16];
+	int digits;
+
+	digits = count_digits(nbr);
+	counter = 0;
+	hex_list = "0123456789ABCDEF";
+	while (nbr / 16 != 0)
+	{
+		remainders[digits] = hex_list[nbr % 16];
+		nbr = nbr / 16;
+		digits--;
+	}
+	remainders[digits] = hex_list[nbr % 16];
+	digits = 0;
+	while (remainders[digits])
+	{
+		ft_putchar(remainders[digits] + '0');
+		digits++;
+	}
+}
+int ft_puthex(unsigned int nbr, int type)
+{
+	int count;
+	char *hex_list;
+
+	count = 0;
+	if (type == 1)
+		hex_list = "0123456789ABCDEF";
+	else
+		hex_list = "0123456789abcdef";
+	if (nbr > 16)
+		count += ft_puthex(nbr / 16, type);
+	else if (nbr > 0)
+	{
+		count = ft_putchar(hex_list[nbr]);
+	}
+	else if (nbr == 0)
+		count += ft_putchar('0');
+	return count;
+}
